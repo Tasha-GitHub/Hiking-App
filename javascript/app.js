80e908cd234fc14730cb92477d6ab1ec9c8080c8
jessephilip <jessephilip@gmail.com>
2016-11-16 07:08:46 -0600

    cleaned up the .js file and added a text file for .js notes

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,5 @@
+// ========== Begin google maps code ==========
+
 // ---------- GLOBAL VARIABLES ----------
 // variable to hold the navigator.geolocation coordinates
 var userLocation;
@@ -9,93 +11,54 @@ var searchURL = "https://maps.googleapis.com/maps/api/js?";
 // this invokes the distance-matrix Google API
 var distanceURL = "https://maps.googleapis.com/maps/api/distancematrix/json?";
 
-    /*
-    Geocoding is the process of converting addresses (like "1600 Amphitheatre Parkway, Mountain View, CA") into geographic coordinates (like latitude 37.423021 and longitude -122.083739), which you can use to place markers on a map, or position the map.
-    */
-
-    /*
-    The process of doing the opposite, translating a location on the map into a human-readable address, is known as reverse geocoding.
-    */
-
 // this url invokes the geocoding Google API
 var geocodingURL = "https://maps.googleapis.com/maps/api/geocode/json?";
-    
-    // address is required
-    var addressTag = "&address=";
-    var componentsTag = "&components=";
-    var boundsTag = "&bounds=";
-    var languageTag = "&language=";
-    var regionTag = "&region=";
-    var latlngTag = "&latlng";
-    var place_idTag = "&place_id";
-    var result_typeTag = "&result_type";
-    var location_typeTag = "&location_type";
+
+// address is required
+var addressTag = "&address=";
+var componentsTag = "&components=";
+var boundsTag = "&bounds=";
+var languageTag = "&language=";
+var regionTag = "&region=";
+var latlngTag = "&latlng";
+var place_idTag = "&place_id";
+var result_typeTag = "&result_type";
+var location_typeTag = "&location_type";
 
 var placesURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
-    
-    //required
-    var locationTag = "&location=";
-    // one or the other
-    var radiusTag = "&radius=";
-    var rankbyDistanceTag = "&rankby=distance";
-    //optional
-    var keywordTag = "&keyword=";
-    // &language=
-    var minpriceTag = "&minprice=";
-    var maxpriceTag = "&maxprice=";
-    var nameTag = "&name=";
-    var opennowTag = "&opennow=";
-    var rankbyTag = "&rankby=";
-    var typeTag = "&type=";
-    var pagetoken = "&pagetoken=";
 
+// locationTag is required required
+var locationTag = "&location=";
+// one or the other
+var radiusTag = "&radius=";
+var rankbyDistanceTag = "&rankby=distance";
+//optional
+var keywordTag = "&keyword=";
+// &language=
+var minpriceTag = "&minprice=";
+var maxpriceTag = "&maxprice=";
+var nameTag = "&name=";
+var opennowTag = "&opennow=";
+var rankbyTag = "&rankby=";
+var typeTag = "&type=";
+var pagetokenTag = "&pagetoken=";
 
-// object containing google api 
-
-
-
-
-    geoCoding: {
-
-        parameters: {
-            // required address or components
-            
-            /*
-            The street address that you want to geocode, in the format used by the national postal service of the country concerned. Additional address elements such as business names and unit, suite or floor numbers should be avoided.
-            */
-            
-        }
-    },
-    apiKey: "&key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8",
-    callback: "&callback=", // initMap
-}
+// commmon tags to each google api
+var keyTag = "&key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8";
+var callbackTag = "&callback="; // initMap
 
 // ---------- LOCATORS ----------
 //the submit button
 var submitButton = $("#submitButton");
 
 // ---------- CLICKLISTENERS ----------
-//https://maps.googleapis.com/maps/api/js?key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8&callback=initMap"
 
 submitButton.on("click", function(e) {
     e.preventDefault();
 
     getLocation();
     initMap();
-
-
-
 })
-
-
-/*$.ajax({
-        url: googleAPI.urls.search + googleAPI.apiKey + googleAPI.callback + "initMap",
-        type: "GET",
-        dataType: "json",
-    })
-    .done(function(object) {
-        console.log(object);
-    });*/
 
 // ---------- FUNCTIONS ----------
 
@@ -129,11 +92,9 @@ function startUp() {
 // ---------- STARTUP CODE ----------
 startUp();
 
-/*
-<!-- this script runs a function whenever it is loaded -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8&callback=initMap"
-    async defer></script>*/
-// ================================================
+
+
+// ================================================ Begin openWeather code
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
 $('#submitButton').on('click', function() {
