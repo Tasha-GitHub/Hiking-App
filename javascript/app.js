0fa439cdbfa2fea6774fe0d66e1cd06c60dcaeb8
jessephilip <jessephilip@gmail.com>
2016-11-15 22:50:07 -0600

    added a lot of variables for multiple API queries

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,70 @@
 // ---------- GLOBAL VARIABLES ----------
-
 // variable to hold the navigator.geolocation coordinates
 var userLocation;
 
+// ========== URLS and QUERY TERMS ==========
+// not sure what this one is used for yet. I think to create a map
+var searchURL = "https://maps.googleapis.com/maps/api/js?";
+
+// this invokes the distance-matrix Google API
+var distanceURL = "https://maps.googleapis.com/maps/api/distancematrix/json?";
+
+    /*
+    Geocoding is the process of converting addresses (like "1600 Amphitheatre Parkway, Mountain View, CA") into geographic coordinates (like latitude 37.423021 and longitude -122.083739), which you can use to place markers on a map, or position the map.
+    */
+
+    /*
+    The process of doing the opposite, translating a location on the map into a human-readable address, is known as reverse geocoding.
+    */
+
+// this url invokes the geocoding Google API
+var geocodingURL = "https://maps.googleapis.com/maps/api/geocode/json?";
+    
+    // address is required
+    var addressTag = "&address=";
+    var componentsTag = "&components=";
+    var boundsTag = "&bounds=";
+    var languageTag = "&language=";
+    var regionTag = "&region=";
+    var latlngTag = "&latlng";
+    var place_idTag = "&place_id";
+    var result_typeTag = "&result_type";
+    var location_typeTag = "&location_type";
+
+var placesURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+    
+    //required
+    var locationTag = "&location=";
+    // one or the other
+    var radiusTag = "&radius=";
+    var rankbyDistanceTag = "&rankby=distance";
+    //optional
+    var keywordTag = "&keyword=";
+    // &language=
+    var minpriceTag = "&minprice=";
+    var maxpriceTag = "&maxprice=";
+    var nameTag = "&name=";
+    var opennowTag = "&opennow=";
+    var rankbyTag = "&rankby=";
+    var typeTag = "&type=";
+    var pagetoken = "&pagetoken=";
+
+
 // object containing google api 
-var googleAPI = {
-    urls: {
-        search: "https://maps.googleapis.com/maps/api/js?"
+
+
+
+
+    geoCoding: {
+
+        parameters: {
+            // required address or components
+            
+            /*
+            The street address that you want to geocode, in the format used by the national postal service of the country concerned. Additional address elements such as business names and unit, suite or floor numbers should be avoided.
+            */
+            
+        }
     },
     apiKey: "&key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8",
     callback: "&callback=", // initMap
@@ -20,12 +78,12 @@ var submitButton = $("#submitButton");
 //https://maps.googleapis.com/maps/api/js?key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8&callback=initMap"
 
 submitButton.on("click", function(e) {
-	e.preventDefault();
+    e.preventDefault();
 
-	getLocation();
-	initMap();
+    getLocation();
+    initMap();
 
-	
+
 
 })
 
@@ -73,7 +131,7 @@ startUp();
 
 /*
 <!-- this script runs a function whenever it is loaded -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8&callback=initMap"
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNpDZ-opNGQ_O4Tj5Fh9JaymUItYJ60b8&callback=initMap"
     async defer></script>*/
 // ================================================
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
