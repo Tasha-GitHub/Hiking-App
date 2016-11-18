49574524b0e499fc7c2a0ac05c5a0486d03f3d8b
Tasha Casagni <Tasha@Tashas-MacBook-Pro.local>
2016-11-18 09:44:10 -0600

    added function to opentrails.js

--- javascript/openTrail.js
+++ javascript/openTrail.js
@@ -1,28 +1,46 @@
-//var city = googleMapsCity;
-var city = "austin";
-var queryURL = 'https://trailapi-trailapi.p.mashape.com/?q[city_cont]='+ city;
+
+//----------------------------------------------------------//
+//                  open trails API controls                //
+//----------------------------------------------------------//
+
+var city;
+//var city = "austin";
+var queryURL;
 var park;
 var url;
-//need to add click event to trigger api call
-$.ajax({
-    url: queryURL, // The URL to the API. You can get this in the API page of the API you intend to consume
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {}, // Additional parameters here
-    dataType: 'json',
-    success: function(data) { console.dir((data.source)); },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "xLa3MQj6eHmshOkWRkpAEiiNTzl0p1n6HbpjsnOvwImXNqnMfQ"); // Enter here your Mashape key
-    }
-}).done(function(response){
-    	console.log(response.places[2].activities);
-    	for(var i = 1; i<=5; i++){
-    	park = response.places[i].name;
-    	$(".availableTrails").append("<div class=\"trail\" data-name=\""+park+"\" id=\""+"item-"+i+"\">"+"<p>"+park+"</p></div>");
-    	}
-    });
 
-// controls for main navbar clicks
+
+function openTrailsAPI(){
+    console.log("the city is"+ city)
+    city = googleMapsCity;
+    queryURL = 'https://trailapi-trailapi.p.mashape.com/?q[city_cont]='+ city;
+
+    //need to add click event to trigger api call
+    $.ajax({
+        url: queryURL, // The URL to the API. You can get this in the API page of the API you intend to consume
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {}, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {},
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+        xhr.setRequestHeader("X-Mashape-Authorization", "xLa3MQj6eHmshOkWRkpAEiiNTzl0p1n6HbpjsnOvwImXNqnMfQ"); // Enter here your Mashape key
+        }
+    }).done(function(response){
+        	//console.log(response.places[2].activities);
+        	for(var i = 1; i<=5; i++){
+        	park = response.places[i].name;
+        	$(".availableTrails").append("<div class=\"trail\" data-name=\""+park+"\" id=\""+"item-"+i+"\">"+"<p>"+park+"</p></div>");
+        	}
+        });
+
+
+};
+
+//----------------------------------------------------------//
+//           index.html scroll controls                     //
+//----------------------------------------------------------//
+
 
 $("#map").on("click", scrollMapWin);
 
@@ -30,7 +48,7 @@ function scrollMapWin(event) {
     event.preventDefault();
 
     window.scrollTo(0, 1300);
-    console.log("hi")
+    
 }
 
 
@@ -39,6 +57,6 @@ $("#forecast").on("click", scrollForecastWin);
 function scrollForecastWin(event) {
     event.preventDefault();
 
-    window.scrollTo(0, 600);
-    console.log("hi")
+    window.scrollTo(0, 650);
+    
 }
