9976c0359c31723f01e27f8c652119bb4879ee54
Shweta <Shweta>
2016-11-18 09:40:54 -0600

    added code to get current weather based on user location

--- javascript/weatherApp.js
+++ javascript/weatherApp.js
@@ -1,9 +1,85 @@
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
-console.log("test: "+ window.userLocation);
 
-// var queryURLBase = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139
+
+var x = document.getElementById("dayForecast");
+// var latitute = "";
+// var longitude = "";
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        // latitute = position.coords.latitude;
+        // longitude = position.coords.longitude;
+
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+
+}
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude);
+    console.log("Longitude: " + position.coords.longitude);
+
+    $(document).ready(function() {
+        var queryURLBase = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=imperial&appid=" + APIKey;
+        console.log("today: " + queryURLBase);
+
+        $.ajax({
+                url: queryURLBase,
+                method: 'GET'
+            })
+            .done(function(response) {
+                console.log(response);
+                var forecast = $(".locationWeather").append("<div class=\"dayForecast\" ><h4>" + response.name + "</h4><p>Wind: " + response.wind.speed +
+                    " MPH</p><p>Humidity: " + response.main.humidity +
+                    " %</p><p>Pressure: " + response.main.pressure +
+                    " PSI</p><p>Max Temp: " + response.main.temp_max +
+                    " F</p><p>Min Temp: " + response.main.temp_min + " F</p></div>");
+                var p = $(".locationWeather").append(forecast);
+
+            });
+
+        var queryURLBase = "http://api.openweathermap.org/data/2.5/forecast?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&units=imperial&appid=" + APIKey;
+        console.log("forecast: " + queryURLBase);
+
+        $.ajax({
+                url: queryURLBase,
+                method: 'GET'
+            })
+            .done(function(response) {
+                console.log(response);
+                var results = response.list;
+                $(".dailyForecast").empty();
+                for (var i = 2; i < 24; i++) {
+
+                    var date = results[i].dt_txt;
+                    var convertedDate = moment(new Date(date));
+                    var day = moment(convertedDate).format("dddd");
+
+                    var forecast = $(".locationForecast").append("<div class=\"dayForecast\" ><h4>" + day + "</h4><p>Wind: " + results[i].wind.speed +
+                        " MPH</p><p>Humidity: " + results[i].main.humidity +
+                        " %</p><p>Pressure: " + results[i].main.pressure +
+                        " PSI</p><p>Max Temp: " + results[i].main.temp_max +
+                        " F</p><p>Min Temp: " + results[i].main.temp_min + " F</p></div>");
+                    var p = $(".locationForecast").append(forecast);
+                    i = i + 7;
+
+                }
+
+            });
+    });
+
+}
+
+getLocation();
+
+
+
+// var queryURLBase = "http://api.openweathermap.org/data/2.5/weather?lat=" + 35 + "&lon=" + +"&units=imperial&appid=" + APIKey;
 
 $("#weatherSubmitButton").on("click", function(event) {
+    $(".locationWeather").remove();
     event.preventDefault();
     var zipOrCity = $("#weatherZipCode").val();
 
@@ -40,6 +116,7 @@ $("#weatherSubmitButton").on("click", function(event) {
 });
 
 $("#weatherForecastButton").on("click", function(event) {
+    $(".locationForecast").remove();
     event.preventDefault();
     var zipOrCity = $("#weatherZipCode").val();
 
